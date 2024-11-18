c7b9b92844709dd587e9dc9b0f9a10c0c44f2e4f
charan <charan@charan.charan>
2024-11-18 08:23:20 -0500

    minor fix

--- add1.c
+++ add1.c
@@ -2,17 +2,15 @@
 #include <x86intrin.h>
 
 __m256i add1(__m256i v) {
-    // return _mm256_sub_epi32(v, _mm256_cmpeq_epi32(v, v));
-    __m256i y;
-    asm("vpcmpeqd %2, %1, %0" : "=x"(y) /* output */ : "x"(y), "x"(y) /* inputs */);
-    asm("vpsubd %2, %1, %0" : "=x"(y) : "x"(v), "x"(y));
-    return y;
+    return _mm256_sub_epi32(v, _mm256_cmpeq_epi32(v, v));
 }
 
 int main() {
     __m256i v = _mm256_set1_epi32(1);
-    __m256i v1 = add1(v);
-    unsigned int *a = (unsigned int *)&v1;
+    __m256i y; // add1(v)
+    asm("vpcmpeqd %2, %1, %0" : "=x"(y) /* output */ : "x"(v), "x"(v) /* inputs */);
+    asm("vpsubd %2, %1, %0" : "=x"(y) : "x"(v), "x"(y));
+    unsigned int *a = (unsigned int *)&y;
     for (int i = 0; i < 8; i++) {
         printf("%d ", a[i]);
     }
