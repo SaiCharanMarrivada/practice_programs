384da9793d363dd25413cf03c986614126d907b2
charan <charan@charan.charan>
2024-11-25 00:41:18 +0530

    use bench.h for simd_example2.cc

--- simd_example2.cc
+++ simd_example2.cc
@@ -1,28 +1,22 @@
 #include <emmintrin.h>
 #include <immintrin.h>
-#include <stdio.h>
+#include "bench.h"
 #include <x86intrin.h>
 #include <assert.h>
 
-#define N (int)4e6
+#define N (int)4e5
 typedef int Vector8 __attribute__((vector_size(32)));
 
-#ifdef VERSION1
 unsigned int count(float *d, float value, int n) {
-    __m256i value8 = (__m256i)_mm256_set1_ps(value);
     int count = 0;
-
-    for (int i = 0; i < n; i += 8) {
-        __m256 x = _mm256_load_ps(&d[i]);
-        __m256i z = _mm256_cmpeq_epi32((__m256i)x, value8);
-        int mask = _mm256_movemask_ps((__m256)z);
-        count += __builtin_popcount(mask);
+    for (int i = 0; i < n; i++) {
+        if (d[i] == value) count++;
     }
 
     return count;
 }
-#else
-int count(float *d, float value, int n) {
+
+int count1(float *d, float value, int n) {
     __m256i value8 = (__m256i)_mm256_set1_ps(value);
     __m256i s1 = _mm256_setzero_si256();
     __m256i s2 = _mm256_setzero_si256();
@@ -36,31 +30,19 @@ int count(float *d, float value, int n) {
         s2 = _mm256_add_epi32(s2, mask2);
     }
     s1 = _mm256_add_epi32(s1, s2);
-    Vector8 s1v = Vector8(s1);
+    // Vector8 s1v = Vector8(s1);
+    int *v = (int *)&s1;
     int sum = 0;
-    for (int i = 0; i < 8; i++) sum += s1v[i];
+    for (int i = 0; i < 8; i++) sum += v[i];
     return -sum;
 }
 
-#endif
+#define SETUP                   \
+    float d[N];                 \
+    for (int i = 0; i < N; i++) \
+        d[i] = rand() % 100
 
-int main() {
-    float d[N];
-    for (int i = 0; i < N; i++) {
-        d[i] = (float)(rand() % 20);
-    }
+CREATE_BENCHMARK(SETUP, count, d, 11, N);
+CREATE_BENCHMARK(SETUP, count1, d, 11, N);
 
-    for (int i = 0; i < N; i++) {
-        // int value = d[i];
-        int _count = 0;
-        for (int j = 0; j < N; j++) {
-            if (d[j] == d[i]) _count++;
-        }
-
-        if (_count != count(d, d[i], N)) {
-            printf("_count = %d, count(d, d[%d], %d) = %u\n", _count, i, N,
-                count(d, d[i], N));
-        }
-    }
-
-}
+BENCHMARK_MAIN();
