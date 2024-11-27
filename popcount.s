c25622f0d90fa27a81b5bfe6058ad325e5467c8a
charan <charan@charan.charan>
2024-11-27 20:35:50 +0530

    popcount.s

---
+++ popcount.s
@@ -0,0 +1,19 @@
+.global popcount
+
+.section .text
+
+popcount:
+    # not needed as `popcnt` instruction is provided by x86
+    xor %rdx, %rdx
+    test %rdi, %rdi
+    je _end
+count_ones:
+    lea -1(%rdi), %rax
+    inc %rdx
+    and %rax, %rdi
+    jne count_ones
+_end:
+    mov %rdx, %rax
+    ret
+
+.section .note.GNU-stack, "", %progbits
