7acf3115b3f0ff31c3250274e964d8163cca87e0
charan <charan@charan.charan>
2024-11-29 08:19:13 +0530

    r lists

--- lists/target/debug/deps/lists-e071197fb893f648.d
+++
@@ -1,6 +0,0 @@
-/home/charan/practice_programs/lists/target/debug/deps/lists-e071197fb893f648: src/lib.rs src/first.rs
-
-/home/charan/practice_programs/lists/target/debug/deps/lists-e071197fb893f648.d: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
--- lists/target/debug/liblists.d
+++
@@ -1,1 +0,0 @@
-/home/charan/practice_programs/lists/target/debug/liblists.rlib: /home/charan/practice_programs/lists/src/first.rs /home/charan/practice_programs/lists/src/lib.rs
