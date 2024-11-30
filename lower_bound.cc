ffda3b4240ff134ae671de837b2b0cd02ed8a4be
charan <charan@charan.charan>
2024-12-01 00:20:18 +0530

    lower_bound.cc

---
+++ lower_bound.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <stdlib.h>
+#include "bench.h"
+#define N (1 << 19)
+
+using std::lower_bound;
+
+int _lower_bound(int *a, int x, unsigned int n) {
+    int *current = a;
+    unsigned int length = n;
+    while (length > 1) {
+        unsigned int half_length = length / 2;
+        current += (a[half_length - 1] < x) * half_length;
+        length -= half_length;
+    }
+    return *current;
+}
+
+#define SETUP   \
+    int *a = (int *)malloc(N * sizeof(int));   \
+    for (int i = 0; i < N; i++) { \
+        a[i] = rand() % 100; \
+    } \
+    int x = 101;
+
+CREATE_BENCHMARK(SETUP, _lower_bound, a, x, N);
+CREATE_BENCHMARK(SETUP, lower_bound, a, a + N, x);
+
+BENCHMARK_MAIN();
