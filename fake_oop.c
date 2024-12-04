d436be8f75fe7a7bde065dc053fb594f15b3408a
charan <charan@charan.charan>
2024-12-04 06:58:25 +0530

    fake oop

---
+++ fake_oop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct {
+    int x, y;
+} Point2;
+
+typedef struct {
+    Point2 point2;
+    int z;
+} Point3;
+
+void print_point2(Point2 *point2) {
+    printf("%d, %d", point2->x, point2->y);
+}
+
+void print_point3(Point3 *point3) {
+    print_point2((Point2 *)point3);
+    printf(", %d", point3->z);
+}
+
+int main() {
+    Point3 p = {.point2.x = 1, .point2.y = 2, .z = 3};
+    print_point3(&p);
+}
