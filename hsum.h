ea8e8bb973059f5c37048fd47917972a5b681ac1
charan <charan@charan.charan>
2024-12-04 06:04:02 +0530

    hsum.h

---
+++ hsum.h
@@ -0,0 +1,9 @@
+#include <x86intrin.h>
+
+int hsum(__m256i x) {
+    __m128i l = _mm256_extracti128_si256(x, 0);
+    __m128i h = _mm256_extracti128_si256(x, 1);
+    l = _mm_add_epi32(l, h);
+    l = _mm_hadd_epi32(l, l);
+    return _mm_extract_epi32(l, 0) + _mm_extract_epi32(l, 1);
+}
