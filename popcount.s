c9f4b40b7b4c0519aaf9170a7ab631515d92ed2b
charan <charan@charan.charan>
2024-12-29 17:39:15 +0530

    use 'blsr' instruction

--- popcount.s
+++ popcount.s
@@ -2,18 +2,30 @@
 
 .section .text
 
+//popcount:
+//    # not needed as `popcnt` instruction is provided by x86
+//    xor %edx, %edx
+//    test %rdi, %rdi
+//    je _end
+//count_ones:
+//    lea -1(%rdi), %rax
+//    inc %rdx
+//    and %rax, %rdi
+//    jne count_ones
+//_end:
+//    mov %rdx, %rax
+//    ret
+
+/// repeatedly clear the lsb bit and count
 popcount:
-    # not needed as `popcnt` instruction is provided by x86
-    xor %edx, %edx
+    xor %eax, %eax
     test %rdi, %rdi
     je _end
 count_ones:
-    lea -1(%rdi), %rax
-    inc %rdx
-    and %rax, %rdi
+    inc %rax
+    blsr %rdi, %rdi
     jne count_ones
 _end:
-    mov %rdx, %rax
     ret
 
 .section .note.GNU-stack, "", %progbits
